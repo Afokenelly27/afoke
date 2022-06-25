bd8d1cd522486432a58fc8c21c79e13515a363a1
Afokeoghene <97847878+Afokenelly27@users.noreply.github.com>
2022-06-25 06:53:37 +0100

    Update lea.js

--- lea.js
+++ lea.js
@@ -1,2 +1,2 @@
-window.alert('name: Afokeoghene Ojreo Nelly  \n height: 1.63m \n country: Nigeria');
-document.write('name: Afokeoghene Ojreo Nelly  <br/> height: 1.63m <br/> country: Nigeria');
+window.alert('name: Afokeoghene Ojero Nelly  \n height: 1.63m \n country: Nigeria');
+document.write('name: Afokeoghene Ojero Nelly  <br/> height: 1.63m <br/> country: Nigeria');
